f6deff7ff54975a97f23628b8808463126714657
aliyu-bashir <81774115+aliyu-bashir@users.noreply.github.com>
2021-04-01 20:11:22 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -7,7 +7,6 @@ print(name)
 print(department)
 
 print("===^^^^____^^^^===")
-print("===^^^^____^^^^===")
 """
 Assign the string "Boo!" to the variable scare and print the variable.
 """
@@ -15,26 +14,23 @@ scare = "Boo!"
 print(scare)
 
 print("===^^^^____^^^^===")
-print("===^^^^____^^^^===")
 """
 Add two variables and assign the result to a third variable. print all the variable names.
 """
 num1 = 10
 num2 = 5
-result =	num1 + num2
+result = num1 + num2
 print(num1)
 print(num2)
 print(result)
 
 print("===^^^^____^^^^===")
-print("===^^^^____^^^^===")
 """
 Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
 """
-outcome =	12/.5
+outcome = 12/.5
 print(outcome)
 
-print("===^^^^____^^^^===")
 print("===^^^^____^^^^===")
 """
 Assign the sum of two numbers to a variable and print the variable.
@@ -43,14 +39,12 @@ sumValues = 3 + 5
 print(sumValues)
 
 print("===^^^^____^^^^===")
-print("===^^^^____^^^^===")
 """
 Write the code that finds the remainder when 8 is divided by 3
 """
-remainder =	8/3
+remainder = 8/3
 print(remainder)
 
-print("===^^^^____^^^^===")
 print("===^^^^____^^^^===")
 """
 Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result.
@@ -59,5 +53,5 @@ x = 2 * 4 * 4 + 2
 multiple = 2 * 4
 added =	4 + 2
 
-result =	multiple * added
+result = multiple * added
 print(result)
